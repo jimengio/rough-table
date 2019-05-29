b51eae8889359438c2a31c5e369b3d9a14541c22
ChenYong <chenyong@jimeng.io>
2019-05-29 11:28:36 +0800

    add example of selecting a row

--- example/pages/container.tsx
+++ example/pages/container.tsx
@@ -1,4 +1,4 @@
-import React, { SFC } from "react";
+import React, { FC, useState } from "react";
 import { parseRoutePath, IRouteParseResult } from "@jimengio/ruled-router";
 import { css } from "emotion";
 
@@ -23,18 +23,33 @@ const renderChildPage = (routerTree: IRouteParseResult) => {
   return <div>NOTHING</div>;
 };
 
-let Container: SFC<{ router: any }> = (props) => {
-  let actions: IActionLinkItem[] = [{ text: "修改", onClick: () => {} }, { text: "删除", onClick: () => {} }];
-  let data = [
-    { code: "001", name: "螺丝", model: "DDR6", source: "外购", type: "产品" },
-    { code: "003", name: "扳手", model: "DDR6", source: "外购", type: "产品" },
-    { code: "004", name: "堵头", model: "33-36", source: "外购", type: "产品" },
-    { code: "044", name: "软管", model: "HO", source: "外购", type: "产品" },
-  ];
+let data = [
+  { code: "001", name: "螺丝", model: "DDR6", source: "外购", type: "产品" },
+  { code: "003", name: "扳手", model: "DDR6", source: "外购", type: "产品" },
+  { code: "004", name: "堵头", model: "33-36", source: "外购", type: "产品" },
+  { code: "044", name: "软管", model: "HO", source: "外购", type: "产品" },
+];
+
+let actions: IActionLinkItem[] = [{ text: "修改", onClick: () => {} }, { text: "删除", onClick: () => {} }];
+
+let Container: FC<{ router: any }> = (props) => {
+  let [selected, setSelected] = useState<string>(null);
 
   return (
     <div className={styleContainer}>
       <div className={styleTableArea}>
+        <div className={styleTitle}>Click to select a row</div>
+        <RoughDivTable
+          data={data}
+          labels={["物料编号", "名称", "型号", "操作"]}
+          lastColumnWidth={80}
+          rowPadding={60}
+          renderColumns={(item) => {
+            return [item.code, item.name, item.model, <ActionLinks actions={actions} spaced />];
+          }}
+        />
+        <Space height={40} />
+        <div className={styleTitle}>Paginated</div>
         <RoughDivTable
           data={data}
           labels={["物料编号", "名称", "型号", "来源", "类型", "操作"]}
@@ -45,21 +60,8 @@ let Container: SFC<{ router: any }> = (props) => {
           }}
           pageOptions={{ current: 1, total: 100, pageSize: 10, onChange: (x) => {} }}
         />
-
         <Space height={40} />
-
-        <RoughDivTable
-          data={data}
-          labels={["物料编号", "名称", "型号", "操作"]}
-          lastColumnWidth={80}
-          rowPadding={60}
-          renderColumns={(item) => {
-            return [item.code, item.name, item.model, <ActionLinks actions={actions} spaced />];
-          }}
-        />
-
-        <Space height={40} />
-
+        <div className={styleTitle}>Empty</div>
         <RoughDivTable
           data={[]}
           labels={["物料编号", "名称", "型号", "操作"]}
@@ -70,7 +72,25 @@ let Container: SFC<{ router: any }> = (props) => {
             return [item.code, item.name, item.model, <ActionLinks actions={actions} spaced />];
           }}
         />
+        <Space height={40} />
+        <div className={styleTitle}>Click to select a row</div>
+        <RoughDivTable
+          data={data}
+          labels={["物料编号", "名称", "型号", "操作"]}
+          rowKey="code"
+          selectedKeys={[selected]}
+          onRowClick={(record) => {
+            setSelected(record.code);
+          }}
+          lastColumnWidth={80}
+          rowPadding={60}
+          emptyLocale={"暂无数据"}
+          renderColumns={(item) => {
+            return [item.code, item.name, item.model, <ActionLinks actions={actions} spaced />];
+          }}
+        />
       </div>
+      <Space height={40} />
 
       {renderChildPage(props.router)}
     </div>

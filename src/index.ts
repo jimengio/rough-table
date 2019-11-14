adc702309df9054918ffd2a93c61af3fa0d10d3b
ChenYong <chenyong@jimeng.io>
2019-11-14 12:23:29 +0800

    adding docs for tree-table; export types; alpha release

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as RoughTable } from "./rough-table";
-export { default as RoughDivTable } from "./rough-div-table";
+export { default as RoughDivTable, IRoughTableColumn } from "./rough-div-table";
 export { default as ScrollDivTable } from "./scroll-div-table";
-export { default as RoughTreeTable } from "./rough-tree-table";
+export { default as RoughTreeTable, IItemWithChildren } from "./rough-tree-table";
 
 export { default as ActionLinks, IActionLinkItem } from "./action-links";

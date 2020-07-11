bdc068babaa4d85d478ce402bd3e5e86b32a55b9
Jarvis <st445903324@gmail.com>
2020-07-11 10:40:07 +0800

    use recursive render instead of flatten list for tree table

--- src/rough-tree-table.tsx
+++ src/rough-tree-table.tsx
@@ -1,9 +1,9 @@
-import React, { FC, useState, ReactNode } from "react";
+import React, { FC, useState, ReactNode, createContext, useContext, useMemo } from "react";
 import { css, cx } from "emotion";
 import { flatMap } from "lodash-es";
 import { useImmer } from "use-immer";
 
-import RoughDivTable, { IRoughTableColumn } from "./rough-div-table";
+import RoughDivTable, { RoughTableRow, IRoughTableColumn } from "./rough-div-table";
 import { PaginationProps } from "antd/lib/pagination";
 import { Space, row, rowMiddle } from "@jimengio/flex-styles";
 import { ISimpleObject } from "./types";
@@ -100,52 +100,114 @@ let detectFolded = (path: number[], state: ISimpleObject): boolean => {
   return detectFolded(path.slice(0, path.length - 1), state);
 };
 
+let DepthContext = createContext(0);
+interface IFoldContext {
+  hasChildren: boolean;
+  folded: boolean;
+  setFolded(newState: boolean | ((v: boolean) => boolean)): void;
+}
+let FoldContext = createContext<IFoldContext>({ hasChildren: false, folded: false, setFolded() {} });
+
+type RoughTreeTableRowProps<T = any> = FC<{
+  item: T;
+  children?: IItemWithChildren<T>[];
+  depth: number;
+  folded?: boolean;
+  indexMap: WeakMap<object, number>;
+}>;
+
+let RoughTreeTableRow: RoughTreeTableRowProps = (props) => {
+  let hasChildren = props.children?.length > 0;
+  let [folded, setFolded] = useState(false);
+  return (
+    <>
+      <FoldContext.Provider value={{ hasChildren, folded, setFolded }}>
+        {!props.folded && <RoughTableRow record={props.item} index={props.indexMap.get(props.item)} />}
+      </FoldContext.Provider>
+      <DepthContext.Provider value={props.depth + 1}>
+        {props.children?.map((row, idx) => (
+          <RoughTreeTableRow
+            key={idx}
+            item={row.item}
+            children={row.children}
+            depth={props.depth + 1}
+            folded={props.folded || folded}
+            indexMap={props.indexMap}
+          />
+        ))}
+      </DepthContext.Provider>
+    </>
+  );
+};
+
+type HeadColumnProps<T = any> = FC<{
+  value: T[keyof T];
+  record: T;
+  index: number;
+}>;
+
+let HeadColumn: HeadColumnProps = ({ value, index }) => {
+  let depth = useContext(DepthContext);
+  let { hasChildren, folded, setFolded } = useContext(FoldContext);
+  return (
+    <div className={cx(rowMiddle, styleCell)}>
+      <div style={{ width: depth * 28 }} className={styleSpace} />
+      <span
+        className={styleFold}
+        onClick={() => {
+          setFolded((v) => !v);
+        }}
+      >
+        {hasChildren ? (folded ? roughTreePreset.foldedSymbol : roughTreePreset.unfoldedSymbol) : ""}
+      </span>
+      <Space width={8} />
+      {value}
+    </div>
+  );
+};
+
 let RoughTreeTable: RoughTreeTableProps = React.memo((props) => {
-  let [foldingState, updateFoldingState] = useImmer({});
+  // let [foldingState, updateFoldingState] = useImmer({});
 
   /** Methods */
   /** Effects */
   /** Renderers */
 
-  let expandedData = expandChildren(props.data, [], foldingState);
+  // let expandedData = expandChildren(props.data, [], foldingState);
+  let indexMap = useMemo(() => {
+    let index = 0;
+    let weakMap = new WeakMap();
+    const map = (arr) => {
+      arr?.forEach((data) => {
+        weakMap.set(data.item, index);
+        index += 1;
+        map(data.children);
+      });
+    };
+    map(props.data);
+    return weakMap;
+  }, [props.data]);
 
   let headColumn: IRoughTableColumn = {
     ...props.columns[0],
     dataIndex: props.columns[0].dataIndex as any, // TODO
     render: (value, record, idx) => {
-      let treePath = findPathInTree(record, props.data);
-
-      let stringPath = treePath.join("_");
-      let folded = foldingState[stringPath];
-
-      let hasChildren = detectRecordChildren(record, props.data);
-
       if (props.columns[0].render != null) {
         value = props.columns[0].render(value, record, idx);
       }
 
-      return (
-        <div className={cx(rowMiddle, styleCell)}>
-          <div style={{ width: (treePath.length - 1) * 28 }} className={styleSpace} />
-          <span
-            className={styleFold}
-            onClick={() => {
-              updateFoldingState((draft) => {
-                draft[stringPath] = !folded;
-              });
-            }}
-          >
-            {hasChildren ? (folded ? roughTreePreset.foldedSymbol : roughTreePreset.unfoldedSymbol) : ""}
-          </span>
-          <Space width={8} />
-          {value}
-        </div>
-      );
+      return <HeadColumn value={value} record={record} index={idx} />;
     },
   };
   let expandedColumns = [headColumn].concat(props.columns.slice(1));
 
-  return <RoughDivTable {...props} data={expandedData} columns={expandedColumns} />;
+  return (
+    <RoughDivTable {...props} data={null} columns={expandedColumns}>
+      {props.data?.map((row, index) => (
+        <RoughTreeTableRow key={index} item={row.item} children={row.children} depth={0} indexMap={indexMap} />
+      ))}
+    </RoughDivTable>
+  );
 });
 
 export default RoughTreeTable;
